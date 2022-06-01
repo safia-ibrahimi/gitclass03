86f6df463486c478a5c1136cf150e932bc7c279c
safia-ibrahimi <safia.ibrahimi.999@gmail.com>
2022-05-31 19:12:23 -0500

    first commit

---
+++ src/moazzam/gito.java
@@ -0,0 +1,12 @@
+package moazzam;
+
+public class gito {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("lllllllll");
+System.out.println("wwwwwww");
+
+	}
+
+}
